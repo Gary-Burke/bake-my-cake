15f7d8c5da92d8cfafa31a7374d09724807116d9
gary-burke <garylewisburke@gmail.com>
2025-12-17 19:55:02 +0100

    Add app cakebuilder

--- bakemycake/urls.py
+++ bakemycake/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("pages.urls"), name="pages-urls"),
     path('admin/', admin.site.urls),
-
+    path("", include("pages.urls"), name="pages-urls"),
+    path("", include("cakebuilder.urls"), name="cakebuilder-urls"),
 ]
---
+++ cakebuilder/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def cakebuilder_app(request):
+    return HttpResponse("Here we will bake the cake")
